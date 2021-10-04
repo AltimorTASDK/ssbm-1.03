a7a6f8880c47701c233a243500eaf01e18b1bb0a
AltimorTASDK <altimormc@gmail.com>
2021-10-04 05:31:38 -0400

    Only modify SquatRv inputs that match an expected DBOOC input

--- src/controller_fix/squatrv.cpp
+++ src/controller_fix/squatrv.cpp
@@ -1,16 +1,30 @@
-#include "melee/action_state.h"
-#include "melee/constants.h"
 #include "melee/player.h"
-#include <gctypes.h>
+#include "util/coords.h"
 
-extern "C" bool orig_Interrupt_SquatRv(HSD_GObj *gobj);
-extern "C" bool hook_Interrupt_SquatRv(HSD_GObj *gobj)
+bool should_suppress_squatrv(const HSD_GObj *gobj)
 {
-	// Raise SquatRv threshold by 2 values. This would be 6000, but 5900 avoids an ICs desync.
 	const auto *player = gobj->get<Player>();
-	if (player->input.stick.y <= -.5900f)
+
+	// Must be outside deadzone for exactly 1f (intending to dash on next frame)
+	if (player->input.stick.x == 0.f)
+		return false;
+
+	if (player->input.stick_x_hold_time >= 1)
 		return false;
 		
-	AS_041_SquatRv(gobj);
+	// Must be rim coord (quarter circle motion)
+	if (!is_rim_coord(player->input.stick))
+		return false;
+
+	// Raise SquatRv threshold by 2 values. This would be 6000, but 5900 avoids an ICs desync.
+	if (player->input.stick.y > -.5900f)
+		return false;
+	
 	return true;
+}
+
+extern "C" int orig_Interrupt_SquatRv(HSD_GObj *gobj);
+extern "C" int hook_Interrupt_SquatRv(HSD_GObj *gobj)
+{
+	return !should_suppress_squatrv(gobj) && orig_Interrupt_SquatRv(gobj);
 }
