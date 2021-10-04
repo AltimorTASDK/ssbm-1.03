ba855c273103cd2eca393d3592f9d232a65af3d0
AltimorTASDK <altimormc@gmail.com>
2021-10-04 05:53:31 -0400

    Remove shyguys and freeze FD background

---
+++ src/stages/fd.cpp
@@ -0,0 +1,6 @@
+#include "hsd/gobj.h"
+
+extern "C" void hook_Stage_FinalDestination_BackgroundUpdate(HSD_GObj *gobj)
+{
+	// Do nothing
+}
---
+++ src/stages/ys.cpp
@@ -0,0 +1,6 @@
+#include "hsd/gobj.h"
+
+extern "C" void hook_Stage_YoshisStory_ShyGuyUpdate(HSD_GObj *gobj)
+{
+	// Do nothing
+}
