d957b95ba793aab58c6a6a47c53312c33ff55734
AltimorTASDK <altimormc@gmail.com>
2021-10-04 06:25:45 -0400

    Apply cardinal fix to in-game inputs only

--- src/controller_fix/cardinals.cpp
+++ src/controller_fix/cardinals.cpp
@@ -1,24 +1,27 @@
 #include "util/coords.h"
-#include "util/misc.h"
 #include <cmath>
 #include <gctypes.h>
 
-extern "C" void orig_HSD_PadClamp(s8 *x, s8 *y, bool shift, s8 min, s8 max);
-extern "C" void hook_HSD_PadClamp(s8 *x, s8 *y, bool shift, s8 min, s8 max)
+void fix_stick(const vec2c &hw_values, vec2 *stick)
 {
-	const auto abs_x = std::abs(*x);
-	const auto abs_y = std::abs(*y);
+	const auto abs_x = std::abs(hw_values.x);
+	const auto abs_y = std::abs(hw_values.y);
 
 	// Produce 1.0 cardinals when one axis is >= 80 and the other is in the deadzone
-	if (abs_x >= max && abs_y <= deadzone) {
-		*x = copysign_int(max, *x);
-		*y = 0;
-		return;
-	} else if (abs_y >= max && abs_x <= deadzone) {
-		*x = 0;
-		*y = copysign_int(max, *y);
-		return;
+	if (abs_x >= STICK_MAX && abs_y <= DEADZONE) {
+		stick->x = std::copysign(1.f, hw_values.x);
+		stick->y = 0;
+	} else if (abs_y >= STICK_MAX && abs_x <= DEADZONE) {
+		stick->x = 0;
+		stick->y = std::copysign(1.f, hw_values.y);
 	}
-	
-	orig_HSD_PadClamp(x, y, shift, min, max);
+}
+
+extern "C" void apply_cardinal_fix(HSD_GObj *gobj)
+{
+	auto *player = gobj->get<Player>();
+	const auto &pad = get_input(player->port, 0);
+
+	fix_stick(pad.stick, &player->input.stick);
+	fix_stick(pad.cstick, &player->input.cstick);
 }
--- src/util/coords.h
+++ src/util/coords.h
@@ -1,13 +1,32 @@
 #pragma once
 
+#include "hsd/pad.h"
 #include "melee/player.h"
 #include "util/vector.h"
 #include <cmath>
 
 // 0.2750
-constexpr auto deadzone = 22;
+constexpr auto DEADZONE = 22;
 
-bool check_ucf_xsmash(const Player *player);
+// 1.0
+constexpr auto STICK_MAX = 80;
+
+inline int clamp_pad_index(int index)
+{
+	return (HSD_PadLibData.qnum + (index % HSD_PadLibData.qnum)) % HSD_PadLibData.qnum;
+}
+
+inline const PADStatus &get_input(int port, int offset)
+{
+	return HSD_PadLibData.queue[clamp_pad_index(HSD_PadLibData.qread + offset - 1)].stat[port];
+}
+
+inline bool check_ucf_xsmash(const Player *player)
+{
+	const auto &prev_input = get_input(player->port, -2);
+	const auto &current_input = get_input(player->port, 0);
+	return std::abs(current_input.stick.x - prev_input.stick.x) > 75;
+}
 
 inline int abs_coord_to_int(float x)
 {
