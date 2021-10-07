c9502ff604e74e3038bb82a65c5f34897304718d
AltimorTASDK <altimormc@gmail.com>
2021-10-07 06:46:59 -0400

    Set default rules

--- src/rules/values.cpp
+++ src/rules/values.cpp
@@ -1,16 +1,20 @@
 #include "melee/rules.h"
 #include "rules/values.h"
 
-// Set default LGL and ATL values
+// Set default rules
 struct init_rules {
 	init_rules()
 	{
-		auto *rules = GetGameRules();
-		rules->handicap = 0;
-		rules->damage_ratio = 0;
-	
 		// Set rule value bounds
 		RuleValueBounds[Rule_LedgeGrabLimit] = { 0, ledge_grab_limit_values.size() - 1 };
 		RuleValueBounds[Rule_AirTimeLimit]   = { 0, air_time_limit_values.size() - 1 };
+		
+		auto *rules = GetGameRules();
+		rules->mode = Mode_Stock;
+		rules->stock_count = 4;
+		rules->handicap = 0; // LGL
+		rules->damage_ratio = 0; // ATL
+		rules->stock_time_limit = 6;
+		rules->friendly_fire = true;
 	}
 } init_rules;
