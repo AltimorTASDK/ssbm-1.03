d13570945ca0bc7c2ef4f2d1cad58edf1f63a094
AltimorTASDK <altimormc@gmail.com>
2021-10-09 05:52:35 -0400

    Extra material structs

--- src/hsd/mobj.h
+++ src/hsd/mobj.h
@@ -4,6 +4,21 @@ struct HSD_AObjDesc;
 struct HSD_RenderAnim;
 struct HSD_TexAnim;
 
+struct HSD_ChanAnim {
+	HSD_ChanAnim *next;
+	HSD_AObjDesc *aobjdesc;
+};
+
+struct HSD_TevRegAnim {
+	HSD_TevRegAnim *next;
+	HSD_AObjDesc *aobjdesc;
+};
+
+struct HSD_RenderAnim {
+	HSD_ChanAnim *chananim;
+	HSD_TevRegAnim *reganim;
+};
+
 struct HSD_MatAnim {
 	HSD_MatAnim *next;
 	HSD_AObjDesc *aobjdesc;
