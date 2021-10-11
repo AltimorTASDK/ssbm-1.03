fa137c0c0b93ba69d14490cb482ea89e5c9d9b33
AltimorTASDK <altimormc@gmail.com>
2021-10-11 05:39:21 -0400

    More accurate DL spawns Adjust FoD spawns for new platform heights Use neutral spawns in training mode

--- src/melee/scene.h
+++ src/melee/scene.h
@@ -2,11 +2,18 @@
 
 #include <gctypes.h>
 
+enum Scene {
+	Scene_Menu         = 1,
+	Scene_VsMode       = 2,
+	Scene_Tournament   = 0x1B,
+	Scene_Training     = 0x1C
+};
+
 extern "C" {
 
 extern u8 SceneMajor;
 extern u8 SceneMinor;
 
-bool SinglePlayerModeCheck();
+bool IsSinglePlayerMode();
 
 }
