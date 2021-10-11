f451fea3eebe9e5593c99ebe9bd96d9e25aa1865
AltimorTASDK <altimormc@gmail.com>
2021-10-11 19:09:10 -0400

    close CSS port on unplug

--- src/qol/patches.cpp
+++ src/qol/patches.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 
 extern "C" void Camera_CStickControl();
+extern "C" void CSS_Think();
 
 constexpr u32 NOP = 0x60000000;
 constexpr u32 BLR = 0x4E800020;
@@ -14,6 +15,9 @@ const auto patches = std::array {
 	std::pair { (char*)PlayerThink_Input+0x194, NOP },
 	// Disable cstick camera control
 	std::pair { (char*)Camera_CStickControl,    BLR },
+	// Close CSS port on unplug
+	// li r6, 3
+	std::pair { (char*)CSS_Think+0x35C,         0x38C00003u },
 };
 
 struct apply_patches {
