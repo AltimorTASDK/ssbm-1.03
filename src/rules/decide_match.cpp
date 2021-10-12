b48285ae7b4fb9b8d02795cd8ee26bad0eea62e2
AltimorTASDK <altimormc@gmail.com>
2021-10-12 06:04:54 -0400

    Add teams win conditions

--- src/rules/decide_match.cpp
+++ src/rules/decide_match.cpp
@@ -87,38 +87,76 @@ static int get_player_scores(MatchController *match, score *scores, bool *violat
 	return count;
 }
 
-static void set_player_result(MatchController *match, int slot, bool win)
+// Returns count
+static int get_team_scores(MatchController *match, score *team_scores, bool *violation)
 {
-	match->players[slot].is_big_loser = !win;
-	if (win) {
-		match->winners[match->winner_count] = (u8)slot;
-		match->winner_count++;
+	score player_scores[6];
+	const auto players = get_player_scores(match, player_scores, violation);
+
+	auto count = 0;
+	bool team_exists[5] = { false };
+
+	for (auto i = 0; i < players; i++) {
+		const auto team = match->players[player_scores[i].id].team;
+		
+		if (!team_exists[team]) {
+			team_scores[count] = { .id = team };
+			team_exists[team] = true;
+			count++;
+		}
+			
+		team_scores[team] = add_score(team_scores[team], player_scores[i]);
+	}
+	
+	return count;
+}
+
+static void set_result(MatchController *match, bool teams, int slot, bool win)
+{
+	if (teams) {
+		match->teams[slot].is_big_loser = !win;
+		if (win) {
+			match->team_winners[match->team_winner_count] = (u8)slot;
+			match->team_winner_count++;
+		}
+	} else {
+		match->players[slot].is_big_loser = !win;
+		if (win) {
+			match->winners[match->winner_count] = (u8)slot;
+			match->winner_count++;
+		}
 	}
 }
 
-static bool decide_winners(MatchController *match)
+static bool decide_winners(MatchController *match, bool teams)
 {
 	// Only apply for timeouts
 	if (match->result != MatchResult_Timeout)
 		return false;
 		
-	// Don't check singles win conditions in teams
-	if(match->is_teams)
+	// Don't check singles win conditions in teams and vice versa
+	if(match->is_teams != teams)
 		return false;
 
 	score scores[6];
 	bool violation;
-	const auto count = get_player_scores(match, scores, &violation);
+	const auto count = teams ? get_team_scores(match, scores, &violation)
+	                         : get_player_scores(match, scores, &violation);
 
 	// We only care about the highest score
 	std::partial_sort(scores, scores + 1, scores + count, is_score_better);
 	
 	// Everyone tied for first wins
-	set_player_result(match, scores[0].id, true);
+	if (teams)
+		match->team_winner_count = 0;
+	else
+		match->winner_count = 0;
+	
+	set_result(match, teams, scores[0].id, true);
 	
 	for (auto i = 1; i < count; i++) {
 		const auto win = is_score_equal(scores[i], scores[0]);
-		set_player_result(match, scores[i].id, win);
+		set_result(match, teams, scores[i].id, win);
 	}
 		
 	// Custom result type
@@ -128,9 +166,16 @@ static bool decide_winners(MatchController *match)
 	return true;
 }
 
+extern "C" void orig_MatchController_UpdateTeamWinners(MatchController *match);
+extern "C" void hook_MatchController_UpdateTeamWinners(MatchController *match)
+{
+	if (!decide_winners(match, true))
+		orig_MatchController_UpdateTeamWinners(match);
+}
+
 extern "C" void orig_MatchController_UpdateWinners(MatchController *match);
 extern "C" void hook_MatchController_UpdateWinners(MatchController *match)
 {
-	if (!decide_winners(match))
+	if (!decide_winners(match, false))
 		orig_MatchController_UpdateWinners(match);
 }
