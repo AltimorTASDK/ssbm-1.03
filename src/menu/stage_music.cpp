9abbbe6c0aaaf4f1717d644a5026a003df7ba144
AltimorTASDK <altimormc@gmail.com>
2021-10-16 17:23:11 -0400

    fix indentation

--- src/menu/stage_music.cpp
+++ src/menu/stage_music.cpp
@@ -17,7 +17,10 @@ struct ItemMenuData {
 	u8 match_type_major;
 	u8 selected;
 	u8 toggles[31];
-	u8 item_frequency;
+	union {
+		u8 item_frequency;
+		u8 selected_stage;
+	};
 	char pad022;
 	u8 state;
 	HSD_JObj *jobj_tree[7];
@@ -32,24 +35,14 @@ extern "C" ArchiveModel MenMainConIs_Top;
 
 extern "C" HSD_JObj *Menu_GetItemToggle(ItemMenuData *data, u16 index);
 
+int bgm_selection[6] = { -1, -1, -1, -1, -1, -1 };
+
 template<string_literal str, u16 scale = 138>
 static constexpr auto item_text()
 {
 	return array_cat(
 		text_builder::textbox<scale, 138>(),
 		text_builder::ascii<str>(),
-		text_builder::end_textbox(),
-		text_builder::br());
-}
-
-template<string_literal str, u16 scale = 138>
-static constexpr auto item_text_fit()
-{
-	return array_cat(
-		text_builder::textbox<scale, 138>(),
-		text_builder::fit(),
-		text_builder::ascii<str>(),
-		text_builder::end_fit(),
 		text_builder::end_textbox(),
 		text_builder::br());
 }
@@ -85,14 +78,36 @@ constexpr auto text_right = text_builder::build(
 	item_text<"Mushroom Kingdom II", 114>(),
 	item_text<"Mute City">(),
 	item_text<u"Poké Floats">(),
-	item_text<u"Pokémon Stadium", 134>(),
 	item_text<"Princess Peach's Castle", 102>(),
 	item_text<"Rainbow Cruise">(),
 	item_text<"Saria's Theme">(),
 	item_text<"Super Mario Bros. 3", 124>(),
 	item_text<"Venom">(),
 	item_text<"Yoshi's Island">(),
-	item_text<"Yoshi's Island N64">());
+	item_text<"Yoshi's Island N64">(),
+	item_text<"Custom Music">());
+	
+static void set_toggle(ItemMenuData *data, u16 index, bool toggle)
+{
+	data->toggles[index] = toggle;
+
+	auto *jobj = Menu_GetItemToggle(data, index);
+	auto *on_off_switch = HSD_JObjGetFromTreeByIndex(jobj, 2);
+
+	if (toggle)
+		HSD_JObjReqAnimAll(on_off_switch, 1.f);
+	else
+		HSD_JObjReqAnimAll(on_off_switch, 0.f);
+
+	HSD_JObjAnimAll(on_off_switch);
+}
+
+static void change_stage(ItemMenuData *data, u8 stage)
+{
+	// Copy selection for stage
+	for (u16 i = 0; i < 31; i++)
+		set_toggle(data, i, i == bgm_selection[stage]);
+}
 
 extern "C" void orig_Menu_UpdateItemDisplay(HSD_GObj *gobj, bool index_changed, bool value_changed);
 extern "C" void hook_Menu_UpdateItemDisplay(HSD_GObj *gobj, bool index_changed, bool value_changed)
@@ -100,10 +115,29 @@ extern "C" void hook_Menu_UpdateItemDisplay(HSD_GObj *gobj, bool index_changed, 
 	orig_Menu_UpdateItemDisplay(gobj, index_changed, value_changed);
 	
 	auto *data = gobj->get<ItemMenuData>();
-	auto *jobj = Menu_GetItemToggle(data, MenuSelectedIndex);
+	
+	if (MenuSelectedIndex >= 31 && value_changed) {
+		change_stage(data, MenuSelectedValue);
+		return;
+	}
 
 	// Hide selection indicator
+	auto *jobj = Menu_GetItemToggle(data, MenuSelectedIndex);
 	HSD_JObjSetFlagsAll(HSD_JObjGetFromTreeByIndex(jobj, 4), HIDDEN);
+	
+	if (!value_changed)
+		return;
+		
+	if (MenuSelectedValue)
+		bgm_selection[data->selected_stage] = MenuSelectedIndex;
+	else
+		bgm_selection[data->selected_stage] = -1;
+		
+	for (u16 i = 0; i < 31; i++) {
+		// Disable all other songs
+		if (i != MenuSelectedIndex)
+			set_toggle(data, i, false);
+	}
 }
 
 extern "C" void orig_Menu_SetupItemToggles(HSD_GObj *gobj);
@@ -122,6 +156,9 @@ extern "C" void hook_Menu_SetupItemToggles(HSD_GObj *gobj)
 	// Replace item names
 	data->text_left->data = text_left.data();
 	data->text_right->data = text_right.data();
+
+	// Set up toggles
+	change_stage(data, data->selected_stage);
 }
 
 static void apply_texture_mask(u8 *texture, const u8 *mask, int width, int height,
@@ -146,7 +183,7 @@ static void apply_texture_mask(u8 *texture, const u8 *mask, int width, int heigh
 		// Check if in mask bounds
 		if (x >= mask_x && x < mask_x + mask_w &&
 		    y >= mask_y && y < mask_y + mask_h) {
-				texture[i] ^= mask[i];
+			texture[i] ^= mask[i];
 		}
 	}
 }
