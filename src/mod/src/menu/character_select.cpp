fa31cd4c26036520bafc60c1c5bf2b38296b9bf7
AltimorTASDK <altimormc@gmail.com>
2021-11-01 05:46:28 -0400

    Revert ready to fight color

--- src/mod/src/menu/character_select.cpp
+++ src/mod/src/menu/character_select.cpp
@@ -28,33 +28,30 @@ extern "C" struct {
 	ArchiveModel Portrait;
 } *MnSlChrModels;
 
+#if 0
 template<u8 a, u8 b>
-constexpr auto make_fobj_track()
+constexpr auto make_color_track()
 {
 	constexpr auto a_f32 = (f32)a / 255.f;
 	constexpr auto b_f32 = (f32)b / 255.f;
 	
 	return fobj_builder<HSD_A_FRAC_U16, 32768>::write_keys<
 		HSD_A_OP_LIN,
-		fobj_key { a_f32, 10 },
-		fobj_key { a_f32, 15 },
-		fobj_key { b_f32, 15 },
-		fobj_key { a_f32, 15 },
-		fobj_key { b_f32, 15 },
-		fobj_key { a_f32, 15 },
-		fobj_key { b_f32, 15 },
-		fobj_key { a_f32, 15 }>();
+		{ a_f32, 10 }, { a_f32, 15 }, { b_f32, 15 }, { a_f32, 15 },
+		{ b_f32, 15 }, { a_f32, 15 }, { b_f32, 15 }, { a_f32, 15 }>();
 }
 
 constexpr auto color1 = color_rgb(170, 231, 255);
 constexpr auto color2 = color_rgb( 97, 134, 255);
-constexpr auto track_r = make_fobj_track<color1.r, color2.r>();
-constexpr auto track_g = make_fobj_track<color1.g, color2.g>();
-constexpr auto track_b = make_fobj_track<color1.b, color2.b>();
+constexpr auto track_r = make_color_track<color1.r, color2.r>();
+constexpr auto track_g = make_color_track<color1.g, color2.g>();
+constexpr auto track_b = make_color_track<color1.b, color2.b>();
+#endif
 
 extern "C" void orig_CSS_Setup();
 extern "C" void hook_CSS_Setup()
 {
+#if 0
 	// Replace "READY TO FIGHT" color tracks
 	auto *ready_anim = MnSlChrModels->PressStart.matanim_joint->child->next->matanim;
 	auto *fobj_r = ready_anim->texanim->aobjdesc->fobjdesc;
@@ -63,6 +60,7 @@ extern "C" void hook_CSS_Setup()
 	fobj_r->ad = track_r.data();
 	fobj_g->ad = track_g.data();
 	fobj_b->ad = track_b.data();
+#endif
 	
 	// Remove KO stars
 	for (auto i = 0; i < 6; i++)
