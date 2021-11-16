ae14a334d3d6ffa21ed053bc3055c517f99bdf0b
AltimorTASDK <altimormc@gmail.com>
2021-11-16 00:43:08 -0500

    dep file shouldn't be in repo

--- src/loader/dep/src/loader.cpp.d
+++
@@ -1,5 +0,0 @@
-obj/src/loader.cpp.o: src/loader.cpp \
- D:/Tools/devkitPro/libogc/include/ogc/card.h \
- D:/Tools/devkitPro/libogc/include/gctypes.h
-D:/Tools/devkitPro/libogc/include/ogc/card.h:
-D:/Tools/devkitPro/libogc/include/gctypes.h:
