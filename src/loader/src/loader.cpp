843f7109c4347298aea0e02ad148887bb5260a0e
AltimorTASDK <altimormc@gmail.com>
2021-11-15 21:57:58 -0500

    Working on diff system

--- src/loader/src/loader.cpp
+++ src/loader/src/loader.cpp
@@ -21,6 +21,9 @@ extern "C" {
 
 void TRK_flush_cache(const void *start, u32 size);
 
+void *HSD_MemAlloc(u32 size);
+void HSD_Free(void *ptr);
+
 void OSReport(const char *fmt, ...);
 
 void OSRestoreInterrupts(u32 level);
@@ -44,6 +47,8 @@ static void *mod_init = (void*)0x817B1000;
 static card_file file;
 static card_stat stats;
 static OSThreadQueue sleep_queue;
+static void *read_dest;
+static const char *read_file;
 
 extern "C" char __BSS_START__;
 extern "C" char __BSS_SIZE__;
@@ -81,7 +86,7 @@ static void read_callback(s32 chan, s32 result)
 		return;
 	}
 
-	if (s32 error = CARD_Open(chan, "103Code", &file); error < 0) {
+	if (s32 error = CARD_Open(chan, read_file, &file); error < 0) {
 		panic("CARD_Open failed (%d)\n", error);
 		return;
 	}
@@ -91,7 +96,13 @@ static void read_callback(s32 chan, s32 result)
 		return;
 	}
 
-	if (s32 error = CARD_ReadAsync(&file, mod_init, stats.len, 0, card_callback); error < 0) {
+	if (read_dest == nullptr) {
+		// Wake up the main thread
+		OSWakeupThread(&sleep_queue);
+		return;
+	}
+
+	if (s32 error = CARD_ReadAsync(&file, read_dest, stats.len, 0, card_callback); error < 0) {
 		panic("CARD_ReadAsync failed (%d)\n", error);
 		return;
 	}
@@ -107,16 +118,10 @@ static void patch_crash_screen()
 	TRK_flush_cache(patch_location, 4);
 }
 
-extern "C" [[gnu::section(".init")]] void _start()
+u32 card_read(const char *file, void *dest)
 {
-	OSReport("Running 1.03 loader\n");
-
-	patch_crash_screen();
-
-	// Zero out the bussy
-	memset(&__BSS_START__, 0, (size_t)&__BSS_SIZE__);
-
-	InitCardBuffers();
+	read_file = file;
+	read_dest = dest;
 
 	if (s32 error = CARD_MountAsync(0, CardWorkArea, nullptr, read_callback); error < 0) {
 		panic("CARD_MountAsync failed (%d)\n", error);
@@ -128,7 +133,86 @@ extern "C" [[gnu::section(".init")]] void _start()
 
 	CARD_Unmount(0);
 
-	TRK_flush_cache(mod_init, stats.len);
+	return stats.len;
+}
+
+void *alloc_and_read(const char *file)
+{
+	const auto size = card_read(file, nullptr);
+	auto *buf = HSD_MemAlloc(size);
+	card_read(file, buf);
+	return buf;
+}
+
+void apply_diff(const char *base, const char *diff, char *out)
+{
+	enum {
+		CMD_EOF              = 0,
+		CMD_DATA_LEN16       = 247,
+		CMD_DATA_LEN32       = 248,
+		CMD_COPY_POS16_LEN8  = 249,
+		CMD_COPY_POS16_LEN16 = 250,
+		CMD_COPY_POS16_LEN32 = 251,
+		CMD_COPY_POS32_LEN8  = 252,
+		CMD_COPY_POS32_LEN16 = 253,
+		CMD_COPY_POS32_LEN32 = 254
+	};
+
+	auto offset = 0;
+
+	while (*diff != CMD_EOF) {
+		switch (*diff) {
+		default:
+			const auto len = *(u8*)(diff + 1);
+			memcpy(out, diff + 1, len);
+			out += len;
+			diff += 1 + len;
+			break;
+		case CMD_DATA_LEN16:
+			const auto len = *(u16*)(diff + 1);
+			memcpy(out, diff + 3, len);
+			out += len;
+			diff += 3 + len;
+			break;
+		case CMD_DATA_LEN32:
+			const auto len = *(u32*)(diff + 1);
+			memcpy(out, diff + 5, len);
+			out += len;
+			diff += 5 + len;
+			break;
+		case CMD_COPY_POS16_LEN8:
+			const auto pos = *(u16*)(diff + 1);
+			const auto len = *(u8*)(diff + 3);
+			memcpy(out, diff + 5, len);
+			out += len;
+			diff += 4;
+			break;
+		}
+	}
+}
+
+extern "C" [[gnu::section(".init")]] void _start()
+{
+	OSReport("Running 1.03 loader\n");
+
+	patch_crash_screen();
+
+	// Zero out the bussy
+	memset(&__BSS_START__, 0, (size_t)&__BSS_SIZE__);
+
+	InitCardBuffers();
+
+#ifdef NTSC102
+	const auto code_size = card_read("103Code", mod_init);
+#else
+	auto *base = alloc_and_read("103Code");
+#ifdef NTSC100
+	auto *diff = alloc_and_read("103CodeNTSC100");
+#endif
+
+#endif
+
+	TRK_flush_cache(mod_init, code_size);
 
 	// Run mod init
 	OSReport("Running 1.03\n");
---
+++ tools/diff.py
@@ -0,0 +1,94 @@
+import struct
+import sys
+
+# https://www.w3.org/TR/NOTE-gdiff-19970901
+
+CMD_EOF              = 0
+CMD_DATA_LEN16       = 247
+CMD_DATA_LEN32       = 248
+CMD_COPY_POS16_LEN8  = 249
+CMD_COPY_POS16_LEN16 = 250
+CMD_COPY_POS16_LEN32 = 251
+CMD_COPY_POS32_LEN8  = 252
+CMD_COPY_POS32_LEN16 = 253
+CMD_COPY_POS32_LEN32 = 254
+
+def make_diff(a, b, out):
+    offset = 0
+
+    def cmd_data(pos, length):
+        if length == 0:
+            return
+        data = b[pos:pos+length]
+        if length >= 2**16:
+            out.write(struct.pack(">BI", CMD_DATA_LEN32, length))
+        elif length >= CMD_DATA_LEN16:
+            out.write(struct.pack(">BH", CMD_DATA_LEN16, length))
+        else:
+            out.write(struct.pack(">B", length))
+        out.write(data)
+
+    def cmd_copy(pos, length):
+        if pos >= 2**16 and length >= 2**16:
+            out.write(struct.pack(">BII", CMD_COPY_POS32_LEN32, pos, length))
+        elif pos >= 2**16 and length >= 2**8:
+            out.write(struct.pack(">BIH", CMD_COPY_POS32_LEN16, pos, length))
+        elif pos >= 2**16:
+            out.write(struct.pack(">BIB", CMD_COPY_POS32_LEN8, pos, length))
+        elif length >= 2**16:
+            out.write(struct.pack(">BHI", CMD_COPY_POS16_LEN32, pos, length))
+        elif length >= 2**8:
+            out.write(struct.pack(">BHH", CMD_COPY_POS16_LEN16, pos, length))
+        else:
+            out.write(struct.pack(">BHB", CMD_COPY_POS16_LEN8, pos, length))
+
+    data_pos = 0
+    data_len = 0
+
+    while offset < len(b):
+        match_pos = 0
+        match_len = 0
+
+        # Check for a copy large enough to be more efficient
+        for check_len in range(7, len(b) - offset):
+            data = b[offset:offset+check_len]
+            try:
+                index = a.index(data)
+                match_len = check_len
+                match_pos = index
+            except ValueError:
+                break
+
+        if match_len > 0:
+            cmd_data(data_pos, data_len)
+            cmd_copy(match_pos, match_len)
+            offset += match_len
+            data_pos = offset
+            data_len = 0
+
+        offset += 1
+        data_len += 1
+
+    out.write(struct.pack(">B", CMD_EOF))
+
+def main():
+    if len(sys.argv) < 4:
+        print("Usage: diff.py <from> <to> <output>", file=sys.stderr)
+        sys.exit(1)
+
+    from_path = sys.argv[1]
+    to_path = sys.argv[2]
+    out_path = sys.argv[3]
+
+    with open(from_path, "rb") as f:
+        a = f.read()
+
+    with open(to_path, "rb") as f:
+        b = f.read()
+
+    with open(out_path, "wb") as f:
+        make_diff(a, b, f)
+
+
+if __name__ == "__main__":
+    main()
