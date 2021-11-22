4d107261e75db06fc54111ab7696476aa6752224
AltimorTASDK <altimormc@gmail.com>
2021-11-22 16:07:12 -0500

    Fix dashback crashing with sopo in target test

--- src/mod/src/controller_fix/dashback.cpp
+++ src/mod/src/controller_fix/dashback.cpp
@@ -6,17 +6,17 @@
 static void check_dashback(HSD_GObj *gobj)
 {
 	auto *player = gobj->get<Player>();
-	
+
 	if (Player_IsCPU(player))
 		return;
-	
+
 	// Must be tilt turn f2
 	if (player->animation_frame != 2.f)
 		return;
-		
+
 	if (player->as_data.Turn.is_smash_turn)
 		return;
-		
+
 	// Must satisfy vanilla xsmash conditions
 	// UCF v0.74+ doesn't check direction and it allows a UCF only ICs desync
 	if (player->input.stick.x * -player->direction < plco->x_smash_threshold)
@@ -32,12 +32,15 @@ static void check_dashback(HSD_GObj *gobj)
 	player->direction = -player->direction;
 	player->as_data.Turn.is_smash_turn = true;
 	player->as_data.Turn.can_dash = true;
-	
+
 	if (player->character_id != CID_Popo)
 		return;
-	
+
 	// Retroactively write dashback to Nana inputs
 	auto *nana_gobj = PlayerBlock_GetSubCharGObj(player->slot, 1);
+	if (nana_gobj == nullptr)
+		return;
+
 	auto *nana = nana_gobj->get<Player>();
 	nana->popo_data_write->direction = player->direction;
 	nana->popo_data_write->stick = popo_to_nana(player->input.stick);
