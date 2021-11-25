29410cfec8a4e6eafb756f537641033db48b1595
AltimorTASDK <altimormc@gmail.com>
2021-11-24 20:20:12 -0500

    Fix unmatched brace

--- src/mod/src/stages/ys.cpp
+++ src/mod/src/stages/ys.cpp
@@ -5,6 +5,6 @@ extern "C" void orig_Stage_YoshisStory_ShyGuyUpdate(HSD_GObj *gobj);
 extern "C" void hook_Stage_YoshisStory_ShyGuyUpdate(HSD_GObj *gobj)
 {
 	// Do nothing if not original
-	if (!is_stage_frozen(Stage_YS)) {
+	if (!is_stage_frozen(Stage_YS))
 		orig_Stage_YoshisStory_ShyGuyUpdate(gobj);
 }
