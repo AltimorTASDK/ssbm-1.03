ad91a8b7ed319ebe067317b28c20a547f8b7d3c5
AltimorTASDK <altimormc@gmail.com>
2021-11-28 21:22:15 -0500

    print size in compress_resource.py

--- tools/compress_resource.py
+++ tools/compress_resource.py
@@ -24,7 +24,7 @@ def rle_encode(data):
             out += data[i:i+run]
 
         i += run
-        
+
     return out
 
 def index_encode(data, bits, table):
@@ -36,7 +36,7 @@ def index_encode(data, bits, table):
         # Start from most significant bit
         shifted = index << (8 - bits)
         offset = bitpos % 8
-        
+
         if offset == 0:
             out += bytes([shifted])
         else:
@@ -46,7 +46,7 @@ def index_encode(data, bits, table):
                 out += bytes([((shifted << 8) >> offset) & 0xFF])
 
         bitpos += bits
-        
+
     return out
 
 def compress_rle(data):
@@ -57,14 +57,14 @@ def compress_indexed(data):
     unique_bytes = [b for b in range(0x100) if b in data]
     index_size = len(unique_bytes)
     index_bits = (index_size - 1).bit_length()
-    
+
     if index_bits == 8:
         # No point indexing
         return None
-        
+
     # 1 = indexed
     indexed = bytearray(struct.pack(">Bxxx", 1))
-    
+
     # Uncompressed size
     indexed += struct.pack(">I", len(data))
 
@@ -88,8 +88,8 @@ def compress(data):
     if indexed is None or len(rle) < len(indexed):
         return rle
     else:
-        return indexed 
-        
+        return indexed
+
 def get_header_size(extension):
     return {
         '.tex': 6
@@ -99,23 +99,27 @@ def main():
     if len(sys.argv) < 3:
         print("Usage: compress_resource.py <in> <out>", file=sys.stderr)
         sys.exit(1)
-        
+
     in_path = sys.argv[1]
     out_path = sys.argv[2]
-        
+
     with open(in_path, "rb") as f:
         data = f.read()
-        
+
     header_size = get_header_size(os.path.splitext(in_path)[1])
     header = data[:header_size]
     body = data[header_size:]
 
     compressed = compress(body)
-    
-    compression_rate = (1 - (len(compressed) + len(header)) / len(data)) * 100
+
     name = os.path.basename(out_path)
-    print(f"{name} compression rate: {compression_rate:.02f}%")
-        
+    total_size = 2 + len(compressed) + len(header)
+    compression_rate = (1 - total_size / len(data)) * 100
+
+    print(f"{name} compression rate: "
+          f"{compression_rate:.02f}% "
+          f"({total_size:X} b)")
+
     with open(out_path, "wb") as f:
         # Include the size of the header_size field itself
         f.write(struct.pack(">H", header_size + 2))
