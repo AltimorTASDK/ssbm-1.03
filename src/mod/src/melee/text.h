5dcfb9a7083b3427333f6bf49400bfabda0784d1
AltimorTASDK <altimormc@gmail.com>
2022-01-23 19:59:45 -0500

    consistently use gctypes

--- src/mod/src/melee/text.h
+++ src/mod/src/melee/text.h
@@ -97,7 +97,7 @@ Text *Text_Create(u32 sis_id, s32 canvas_id,
 void Text_Free(Text *text);
 
 void Text_SetFromSIS(Text *text, u32 id);
-void Text_SetSubtextColor(Text *text, int subtext, const color_rgba &color);
+void Text_SetSubtextColor(Text *text, s32 subtext, const color_rgba &color);
 
 DevText *DevelopText_Create(u8 id, u16 x, u16 y, s32 width, s32 height, char *buf);
 void DevelopText_Show(HSD_GObj *unused, DevText *text);
@@ -107,7 +107,7 @@ void DevelopText_HideCursor(DevText *text);
 void DevelopText_SetBGColor(DevText *text, const color_rgba &color);
 void DevelopText_SetColorIndex(DevText *text, u8 index);
 void DevelopText_SetTextColor(DevText *text, const color_rgba &color);
-void DevelopText_SetScale(DevText *text, float x, float y);
+void DevelopText_SetScale(DevText *text, f32 x, f32 y);
 
 void DevelopText_Erase(DevText *text);
 void DevelopText_SetCursor(DevText *text, s32 x, s32 y);
