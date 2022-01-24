5d8e6f618feb498f345f7d5bfe68402f7ccf56b4
AltimorTASDK <altimormc@gmail.com>
2022-01-24 07:05:49 -0500

    fix OSD/menu music crashes on old versions of UP

--- src/mod/src/unclepunch.cpp
+++ src/mod/src/unclepunch.cpp
@@ -1,4 +1,5 @@
 #include "hsd/gobj.h"
+#include "melee/scene.h"
 #include "os/card.h"
 #include "util/patch_list.h"
 
@@ -6,6 +7,7 @@ extern "C" void Interrupt_AS_DamageFall(HSD_GObj *gobj);
 extern "C" void Interrupt_AS_Turn(HSD_GObj *gobj);
 extern "C" void AS_235_SpotDodge_YoshiCheck(HSD_GObj *gobj);
 extern "C" void CSS_Setup();
+extern "C" void Scene_Initialize(SceneMinorData *data);
 
 struct check_unclepunch
 {
@@ -18,13 +20,18 @@ struct check_unclepunch
 		patch_list {
 			// Remove UP's built in UCF
 			// stfs f0, 0x2C(r31)
-			std::pair { (char*)Interrupt_AS_Turn+0x50,           0xD01F002Cu },
+			std::pair { (char*)Interrupt_AS_Turn+0x4C,           0xD01F002Cu },
 			// lwz r4, 0x2C(r3)
 			std::pair { (char*)AS_235_SpotDodge_YoshiCheck+0x10, 0x8083002Cu },
 			// mr r4, r24
 			std::pair { (char*)CSS_Setup+0x2230,                 0x38980000u },
 			// cmpw r3, r0
 			std::pair { (char*)Interrupt_AS_DamageFall+0xCC,     0x7C030000u },
+
+			// Increase CSS text heap size in older UP versions to prevent crashes at
+			// OSD/menu music. Newer UP versions have this built in
+			// li r3, 0x4800
+			std::pair { (char*)Scene_Initialize+0x54,            0x38604800u },
 		};
 	}
 } check_unclepunch;
