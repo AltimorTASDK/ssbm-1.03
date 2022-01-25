ccabcfe74f103ffb06f43c4a503efde060da7036
AltimorTASDK <altimormc@gmail.com>
2022-01-24 19:56:09 -0500

    fix ready to fight check on UP

--- src/mod/src/unclepunch.cpp
+++ src/mod/src/unclepunch.cpp
@@ -7,6 +7,7 @@ extern "C" void Interrupt_AS_DamageFall(HSD_GObj *gobj);
 extern "C" void Interrupt_AS_Turn(HSD_GObj *gobj);
 extern "C" void AS_235_SpotDodge_YoshiCheck(HSD_GObj *gobj);
 extern "C" void CSS_Setup();
+extern "C" void CSS_ReadyThink(HSD_GObj *gobj);
 extern "C" void Scene_Initialize(SceneMinorData *data);
 
 struct check_unclepunch
@@ -27,6 +28,10 @@ struct check_unclepunch
 			std::pair { (char*)CSS_Setup+0x2230,                 0x38980000u },
 			// cmpw r3, r0
 			std::pair { (char*)Interrupt_AS_DamageFall+0xCC,     0x7C030000u },
+
+			// Get rid of UP's 1p start code because it conflicts with 1.03's
+			// cmpwi r4, 2
+			std::pair { (char*)CSS_ReadyThink+0x120,             0x2C040002u },
 
 			// Increase CSS text heap size in older UP versions to prevent crashes at
 			// OSD/menu music. Newer UP versions have this built in
