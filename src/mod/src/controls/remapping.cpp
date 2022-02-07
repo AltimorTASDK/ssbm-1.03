630b8ec1688ecd3e4a31b47985ef0c8e5ff6eb72
AltimorTASDK <altimormc@gmail.com>
2022-02-06 22:01:16 -0500

    don't reset config if in-game

--- src/mod/src/controls/remapping.cpp
+++ src/mod/src/controls/remapping.cpp
@@ -12,10 +12,10 @@ constexpr auto REMAP_HOLD_TIME = 60;
 static void apply_remap(int port)
 {
 	const auto &config = controller_configs[port];
-	
+
 	if (config.z_jump_bit == 0)
 		return;
-		
+
 	// Swap X/Y bit with Z bit
 	auto *status = &HSD_PadMasterStatus[port];
 	status->buttons = bit_swap(status->buttons, config.z_jump_bit, __builtin_ctz(Button_Z));
@@ -24,11 +24,17 @@ static void apply_remap(int port)
 extern "C" void process_remapping(int port);
 extern "C" void process_remapping(int port)
 {
-	// Reset config if unplugged
-	if (HSD_PadMasterStatus[port].err != 0) {
+	// Reset config if unplugged and not in-game
+	if (HSD_PadMasterStatus[port].err != 0 && MatchInfo.match_over) {
 		controller_configs[port].reset();
 		return;
 	}
 
 	apply_remap(port);
+}
+
+[[gnu::constructor]] static void set_match_over()
+{
+	// Initialize this field so it can be used to track if in-game
+	GetMatchInfo()->match_over = true;
 }
