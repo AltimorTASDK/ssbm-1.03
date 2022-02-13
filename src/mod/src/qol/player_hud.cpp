f1f0174a443fc28a979503c33eb682ab8bea363a
AltimorTASDK <altimormc@gmail.com>
2022-02-13 18:05:51 -0500

    refer to stock/percent/logo as "player HUD" rather than "stock icon"

--- src/mod/src/qol/player_hud.cpp
+++ src/mod/src/qol/player_hud.cpp
@@ -4,24 +4,24 @@
 #include "util/melee/ports.h"
 #include <gctypes.h>
 
-extern "C" const vec3 &orig_HUD_GetStockIconPosition(u32 port);
-extern "C" const vec3 &hook_HUD_GetStockIconPosition(u32 port)
+extern "C" const vec3 &orig_HUD_GetPlayerHUDPosition(u32 port);
+extern "C" const vec3 &hook_HUD_GetPlayerHUDPosition(u32 port)
 {
 	// Only apply to vs mode
 	if (SceneMajor != Scene_VsMode)
-		return orig_HUD_GetStockIconPosition(port);
+		return orig_HUD_GetPlayerHUDPosition(port);
 
 	const auto sorted = get_sorted_port_index(port);
 	if (sorted == -1)
-		return orig_HUD_GetStockIconPosition(port);
+		return orig_HUD_GetPlayerHUDPosition(port);
 
 	if (MatchInfo_IsTeams() && !is_1v1()) {
 		// Display each team together
 		const auto team = sorted % 2;
 		const auto index = sorted / 2;
-		return orig_HUD_GetStockIconPosition(team * 2 + index);
+		return orig_HUD_GetPlayerHUDPosition(team * 2 + index);
 	}
 
 	// Center to p2/3 positions in singles
-	return orig_HUD_GetStockIconPosition(sorted + 1);
+	return orig_HUD_GetPlayerHUDPosition(sorted + 1);
 }
