c7d3b4bb8d72d1f689bc9537902a9e34bdbfd6e7
AltimorTASDK <altimormc@gmail.com>
2022-03-20 09:47:42 -0400

    don't repeat aspect_ratio_factor expression

--- src/mod/src/qol/widescreen.h
+++ src/mod/src/qol/widescreen.h
@@ -5,5 +5,5 @@ constexpr auto aspect_ratio_factor = 320.f / 219.f;
 constexpr auto aspect_ratio_wide = aspect_ratio_vanilla * aspect_ratio_factor;
 
 // Adjust ortho camera bounds so that 0 to 640 covers a 4:3 physical area
-constexpr auto ortho_left_wide  = 320.f * (1.f - 320.f / 219.f);
-constexpr auto ortho_right_wide = 320.f * (1.f + 320.f / 219.f);
+constexpr auto ortho_left_wide  = 320.f * (1.f - aspect_ratio_factor);
+constexpr auto ortho_right_wide = 320.f * (1.f + aspect_ratio_factor);
