a8c928cff188e5b6c4f15ae280e56def4e34734f
AltimorTASDK <altimormc@gmail.com>
2022-04-23 05:28:06 -0400

    Use a unique manual for the dol/iso version

--- src/mod/src/menu/manual.cpp
+++ src/mod/src/menu/manual.cpp
@@ -18,7 +18,10 @@
 #include <cmath>
 #include <gctypes.h>
 
-#ifdef NOPAL
+#if defined(DOL)
+#include "resources/manual/dol/manual_p1.tex.h"
+#include "resources/manual/dol/manual_p2.tex.h"
+#elif defined(NOPAL)
 #include "resources/manual/a/manual_p1.tex.h"
 #include "resources/manual/a/manual_p2.tex.h"
 #else
