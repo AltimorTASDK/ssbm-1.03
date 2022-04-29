3e6c4b66d7bd84d9988cfa63f76caef6ba8b0f9c
AltimorTASDK <altimormc@gmail.com>
2022-04-29 09:24:54 -0400

    don't adjust grow-down heap multiple times just in case some mod calls OSSetArenaHi again

--- src/mod/src/dol_init.cpp
+++ src/mod/src/dol_init.cpp
@@ -9,6 +9,8 @@ extern "C" void BootScene_Exit(SceneMinorData *data);
 
 extern "C" char __NEW_BASE__;
 
+static bool adjusted_heap;
+
 PATCH_LIST(
 	// Patch end of save file name to match the name normally set by the memcard loader
 	std::pair { &SaveFileName[20],   '_103' },
@@ -23,9 +25,12 @@ extern "C" void hook_OSSetArenaHi(void *hi)
 	// Prevent heap from overrunning mod code
 	orig_OSSetArenaHi(&__NEW_BASE__);
 
-	// Reduce size of grow-down heap so the base remains the same
-	const auto delta = (u32)hi - (u32)&__NEW_BASE__;
-	hsd_default_heaps[2].size -= delta;
+	if (!adjusted_heap) {
+		// Reduce size of grow-down heap so the base remains the same
+		const auto delta = (u32)hi - (u32)&__NEW_BASE__;
+		hsd_default_heaps[2].size -= delta;
+		adjusted_heap = true;
+	}
 }
 
 #endif
