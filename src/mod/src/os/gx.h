7d1a150fb2690cdec533b89f5fbebe83bdf5dd96
AltimorTASDK <altimormc@gmail.com>
2022-05-12 08:50:20 -0400

    Add u16 write for gx_fifo and prevent infinite recursion for unimplemented types

--- src/mod/src/os/gx.h
+++ src/mod/src/os/gx.h
@@ -5,13 +5,16 @@
 
 inline union {
 private:
-	volatile u8 as_u8;
+	volatile u8  as_u8;
+	volatile u16 as_u16;
 	volatile u32 as_u32;
 	volatile f32 as_f32;
 
+	void write_impl(u8  value) { as_u8  = value; }
+	void write_impl(u16 value) { as_u16 = value; }
+	void write_impl(u32 value) { as_u32 = value; }
+	void write_impl(f32 value) { as_f32 = value; }
+
 public:
-	void write(u8 value) { as_u8 = value; }
-	void write(u32 value) { as_u32 = value; }
-	void write(f32 value) { as_f32 = value; }
-	void write(auto ...values) { (write(values), ...); }
+	void write(auto ...values) { (write_impl(values), ...); }
 } *gx_fifo = (decltype(gx_fifo))0xCC008000;
