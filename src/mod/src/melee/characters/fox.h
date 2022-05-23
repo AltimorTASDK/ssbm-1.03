5c1e7a5900bc9b5edbdec7847de0ddd750d9830f
AltimorTASDK <altimormc@gmail.com>
2022-05-23 13:04:44 -0400

    add fox action states

---
+++ src/mod/src/melee/characters/fox.h
@@ -0,0 +1,12 @@
+#pragma once
+
+enum ActionStateFox {
+	AS_Fox_SpecialLwStart = 360,
+	AS_Fox_SpecialLwLoop,
+	AS_Fox_SpecialLwHit,
+	AS_Fox_SpecialLwEnd,
+	AS_Fox_SpecialAirLwStart,
+	AS_Fox_SpecialAirLwLoop,
+	AS_Fox_SpecialAirLwHit,
+	AS_Fox_SpecialAirLwEnd
+};
