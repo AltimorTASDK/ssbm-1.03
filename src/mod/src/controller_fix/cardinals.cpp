9368906be05d1301f418466cd0d3b2d89c1bfa26
AltimorTASDK <altimormc@gmail.com>
2022-09-01 02:01:33 -0400

    mark unit local func as static

--- src/mod/src/controller_fix/cardinals.cpp
+++ src/mod/src/controller_fix/cardinals.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 #include <gctypes.h>
 
-void fix_stick(const vec2c &hw_values, vec2 *stick)
+static void fix_stick(const vec2c &hw_values, vec2 *stick)
 {
 	const auto abs_x = std::abs(hw_values.x);
 	const auto abs_y = std::abs(hw_values.y);
@@ -22,12 +22,12 @@ extern "C" void apply_cardinal_fix(HSD_GObj *gobj)
 {
 	if (get_ucf_type() == ucf_type::ucf)
 		return;
-	
+
 	auto *player = gobj->get<Player>();
-	
+
 	if (Player_IsCPU(player))
 		return;
-	
+
 	const auto &pad = get_input<0>(player->port);
 
 	fix_stick(pad.stick, &player->input.stick);
