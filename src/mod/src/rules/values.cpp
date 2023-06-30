e222c57463402f769972af9db49f766b07526ae5
AltimorTASDK <altimormc@gmail.com>
2023-06-30 12:44:35 -0400

    don't attempt to preserve 1.03 addresses between melee versions not really possible

--- src/mod/src/rules/values.cpp
+++ src/mod/src/rules/values.cpp
@@ -88,11 +88,6 @@ extern "C" void hook_StartMelee(StartMeleeData *data)
 	data->rules.damage_ratio   = 1;
 	data->rules.item_frequency = -1;
 	data->rules.friendly_fire  = true;
-
-	// Prevent friendly_fire and score_display from being accessed by lhz/sth on 1.00 and
-	// making the function smaller than in other versions
-	asm volatile("": : :"memory");
-
 	data->rules.score_display  = false;
 	data->rules.sd_penalty     = -2;
 
