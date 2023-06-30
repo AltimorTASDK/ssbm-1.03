981af97015b8e09979e76f655786643f54f9fe19
AltimorTASDK <altimormc@gmail.com>
2023-06-30 19:38:02 -0400

    WIP debug mode menu

--- src/mod/src/melee/text.h
+++ src/mod/src/melee/text.h
@@ -115,6 +115,7 @@ void Text_SetFromSIS(Text *text, u32 id);
 void Text_SetSubtextColor(Text *text, s32 subtext, const color_rgba &color);
 
 DevText *DevelopText_Create(u8 id, u16 x, u16 y, s32 width, s32 height, char *buf);
+void DevelopText_Remove(DevText **text);
 void DevelopText_Show(HSD_GObj *unused, DevText *text);
 
 void DevelopText_ShowBackground(DevText *text);
--- src/mod/src/menu/debug_menu.cpp
+++ src/mod/src/menu/debug_menu.cpp
@@ -1,18 +1,20 @@
+#include "hsd/cobj.h"
 #include "hsd/dobj.h"
 #include "hsd/gobj.h"
 #include "hsd/jobj.h"
+#include "hsd/memory.h"
+#include "melee/camera.h"
 #include "melee/debug.h"
 #include "melee/text.h"
 #include "util/patch_list.h"
+#include "util/math.h"
 #include "util/mempool.h"
 #include "util/texture_swap.h"
+#include "util/draw/render.h"
 #include <array>
 
-extern "C" HSD_GObj *LanguageMenuGObj;
-
-extern "C" void Menu_LanguageMenuInput(HSD_GObj *gobj);
-
-constexpr auto anim_frames = std::array { 1.f, 0.f, 0.f, 0.f, 0.f };
+//#include "resources/debug/flag_develop.tex.h"
+//#include "resources/debug/flag_master.tex.h"
 
 struct LanguageMenuData {
 	u8 setting;
@@ -21,6 +23,65 @@ struct LanguageMenuData {
 	Text *text;
 };
 
+extern "C" HSD_GObj *LanguageMenuGObj;
+
+extern "C" void Menu_LanguageMenuInput(HSD_GObj *gobj);
+
+constexpr auto TEXT_WIDTH  = 16;
+constexpr auto TEXT_HEIGHT = 5;
+
+constexpr auto TEXT_SCALE_X = 12;
+constexpr auto TEXT_SCALE_Y = 18;
+
+// Create the illusion of the same coordinate space + Z offset as the existing UI
+constexpr auto TEXT_COORD_SCALE = 21.f;
+
+constexpr auto TEXT_GAP       = (int)(4 * TEXT_COORD_SCALE);
+constexpr auto TEXT_MASTER_X  = 0;
+constexpr auto TEXT_DEVELOP_X = TEXT_MASTER_X + TEXT_WIDTH * TEXT_SCALE_X + TEXT_GAP;
+
+// Center the camera in the gap between the text
+constexpr auto TEXT_MASTER_RIGHT = TEXT_MASTER_X + TEXT_WIDTH * TEXT_SCALE_X;
+constexpr auto TEXT_DEVELOP_LEFT = TEXT_DEVELOP_X - 8;
+
+constexpr auto CAMERA_X = (TEXT_MASTER_RIGHT + TEXT_DEVELOP_LEFT) / 2.f;
+constexpr auto CAMERA_Y = 80.f;
+constexpr auto CAMERA_Z = 32.5f * TEXT_COORD_SCALE;
+
+constexpr auto INTEREST_Z = -18.5f * TEXT_COORD_SCALE;
+
+constexpr auto text_eye      = HSD_WObjDesc { .pos = { CAMERA_X, CAMERA_Y, CAMERA_Z   } };
+constexpr auto text_interest = HSD_WObjDesc { .pos = { CAMERA_X, CAMERA_Y, INTEREST_Z } };
+
+static constinit auto text_cobjdesc = HSD_CObjDesc {
+	.projection_type = ProjType_Perspective,
+	.viewport_right  = 640,
+	.viewport_bottom = 480,
+	.scissor_right   = 640,
+	.scissor_bottom  = 480,
+	.eye_position    = &text_eye,
+	.interest        = &text_interest,
+	.roll            = math::pi,
+	.far             = 65535,
+	.u = {
+		.perspective = {
+			.fov    = rad_to_deg(.725f),
+			.aspect = -4.f/3.f
+		}
+	}
+};
+
+static mempool pool;
+static DevText *text_master;
+static DevText *text_develop;
+static HSD_GObj *text_camera;
+static char text_master_buf[TEXT_WIDTH * TEXT_HEIGHT * 2];
+static char text_develop_buf[TEXT_WIDTH * TEXT_HEIGHT * 2];
+
+// Flag textures
+//static texture texture_flag_master;
+//static texture texture_flag_develop;
+
 PATCH_LIST(
 	// Return to Vs Menu
 	// li r3, MenuType_VsMode
@@ -28,6 +89,18 @@ PATCH_LIST(
 	// li r4, 2
 	std::pair { Menu_LanguageMenuInput+0x90, 0x38800002u }
 );
+
+static void pool_free(void *data)
+{
+	HSD_Free(data); // Default free gobj data
+
+	if (pool.dec_ref() == 0) {
+		DevelopText_Remove(&text_master);
+		DevelopText_Remove(&text_develop);
+		GObj_Free(text_camera);
+		text_camera = nullptr;
+	}
+}
 
 extern "C" void orig_Menu_SetupLanguageMenu(u8 state);
 extern "C" void hook_Menu_SetupLanguageMenu(u8 state)
@@ -65,4 +138,47 @@ extern "C" void hook_Menu_SetupLanguageMenu(u8 state)
 	HSD_DObjGetByIndex<6>(jobj->u.dobj)->flags |= DOBJ_HIDDEN;
 	HSD_DObjGetByIndex<7>(jobj->u.dobj)->flags |= DOBJ_HIDDEN;
 	HSD_DObjGetByIndex<8>(jobj->u.dobj)->flags |= DOBJ_HIDDEN;
+
+	// Free assets on menu exit
+	LanguageMenuGObj->user_data_remove_func = pool_free;
+
+	if (pool.inc_ref() != 0)
+		return;
+
+	text_master = DevelopText_Create(0x6A, TEXT_MASTER_X, 0,
+	                                 TEXT_WIDTH, TEXT_HEIGHT, text_master_buf);
+	DevelopText_HideCursor(text_master);
+	//DevelopText_SetBGColor(text_master, color_rgba::hex(0x40508080u));
+	DevelopText_SetBGColor(text_master, color_rgba::hex(0x40508080u));
+	DevelopText_SetTextColor(text_master, color_rgba::hex(0xFF8020FFu));
+	DevelopText_SetScale(text_master, TEXT_SCALE_X, TEXT_SCALE_Y);
+	DevelopText_SetCursor(text_master, 0, TEXT_HEIGHT / 2);
+	DevelopText_Print(text_master, "DbLevel: Master");
+	DevelopText_Show(nullptr, text_master);
+
+	text_develop = DevelopText_Create(0x6B, TEXT_DEVELOP_X, 0,
+	                                  TEXT_WIDTH, TEXT_HEIGHT, text_develop_buf);
+	DevelopText_HideCursor(text_develop);
+	DevelopText_SetBGColor(text_develop, color_rgba::hex(0x40508080u));
+	//DevelopText_SetTextColor(text_develop, color_rgba::hex(0xFF8020FFu));
+	DevelopText_SetTextColor(text_develop, color_rgba::hex(0xE2E2E2FFu));
+	DevelopText_SetScale(text_develop, TEXT_SCALE_X, TEXT_SCALE_Y);
+	DevelopText_SetCursor(text_develop, 0, TEXT_HEIGHT / 2);
+	DevelopText_Print(text_develop, "DbLevel: Develop");
+	DevelopText_Show(nullptr, text_develop);
+
+	text_camera = GObj_Create(GOBJ_CLASS_CAMERA, GOBJ_PLINK_MENU_CAMERA, 0);
+	GObj_InitKindObj(text_camera, GOBJ_KIND_CAMERA, HSD_CObjLoadDesc(&text_cobjdesc));
+
+	//pool.add(init_texture(flag_develop_tex_data, &texture_flag_develop));
+	//pool.add(init_texture(flag_master_tex_data, &texture_flag_master));
+}
+
+extern "C" void orig_DevelopText_SetupCObj();
+extern "C" void hook_DevelopText_SetupCObj()
+{
+	if (text_camera != nullptr)
+		HSD_CObjSetCurrent(text_camera->get_hsd_obj<HSD_CObj>());
+	else
+		orig_DevelopText_SetupCObj();
 }
