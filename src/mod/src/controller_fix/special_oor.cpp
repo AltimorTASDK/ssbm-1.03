698de75f9425459903465f7bc769d6bfd2bce72d
AltimorTASDK <altimormc@gmail.com>
2023-11-26 23:24:34 -0500

    add special OoR fix

---
+++ src/mod/src/controller_fix/special_oor.cpp
@@ -0,0 +1,23 @@
+#include "dolphin/pad.h"
+#include "melee/action_state.h"
+#include "melee/constants.h"
+#include "melee/player.h"
+#include "rules/values.h"
+
+extern "C" bool orig_Player_CheckSideBInput(Player *player);
+extern "C" bool hook_Player_CheckSideBInput(Player *player)
+{
+	if (!(player->input.instant_buttons & Button_B))
+		return false;
+
+	if (get_ucf_type() != ucf_type::ucf) {
+		// Make neutral/down B adjacent to the Run region
+		switch (player->action_state) {
+		case AS_Dash:
+		case AS_Run:
+			return std::abs(player->input.stick.x) >= plco->run_threshold;
+		}
+	}
+
+	return std::abs(player->input.stick.x) >= plco->x_special_threshold;
+}
